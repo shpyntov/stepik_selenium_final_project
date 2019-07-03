01665b15f01be7a7c9b783cad6dbc1ca3ac22ae7
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2019-07-03 19:05:38 +0300

    delete many promo tests

--- test_product_page.py
+++ test_product_page.py
@@ -8,24 +8,13 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('link', ['http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear',
-                                  'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019',
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(strict=True)),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_cart(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
     page.item_added_message_check()
     page.basket_price_message_check()
+
 
 @pytest.mark.xfail(strict=True)
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
